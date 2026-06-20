33c6b4f154b2026062400f12e84b4ed2ffdf8fc4
Andrei Stan <andreistan275@gmail.com>
2026-06-20 22:27:13 +0300

    Update .config/nvim/lua/config/keymaps.lua Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,76 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+-- Neovide terminal-like copy/paste + font zoom
+if vim.g.neovide then
+  local function paste()
+    vim.api.nvim_paste(vim.fn.getreg("+"), true, -1)
+  end
+
+  local function zoom_in()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * 1.1
+  end
+
+  local function zoom_out()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor / 1.1
+  end
+
+  local function zoom_reset()
+    vim.g.neovide_scale_factor = 1.0
+  end
+
+  -- Copy like a Linux terminal: Ctrl+Shift+C
+  --
+  -- In visual mode: copies selected text.
+  -- In normal mode: copies the current line.
+  vim.keymap.set("x", "<S-C-c>", '"+y', {
+    desc = "Copy selection to system clipboard",
+    silent = true,
+  })
+
+  vim.keymap.set("n", "<S-C-c>", '"+yy', {
+    desc = "Copy current line to system clipboard",
+    silent = true,
+  })
+
+  -- Paste like a Linux terminal: Ctrl+Shift+V
+  --
+  -- Works in normal, insert, visual, command, and terminal mode.
+  vim.keymap.set({ "n", "i", "x", "c", "t" }, "<S-C-v>", paste, {
+    desc = "Paste from system clipboard",
+    silent = true,
+  })
+
+  -- Extra terminal-style alternatives:
+  -- Ctrl+Insert = copy, Shift+Insert = paste
+  vim.keymap.set("x", "<C-Insert>", '"+y', {
+    desc = "Copy selection to system clipboard",
+    silent = true,
+  })
+
+  vim.keymap.set("n", "<C-Insert>", '"+yy', {
+    desc = "Copy current line to system clipboard",
+    silent = true,
+  })
+
+  vim.keymap.set({ "n", "i", "x", "c", "t" }, "<S-Insert>", paste, {
+    desc = "Paste from system clipboard",
+    silent = true,
+  })
+
+  -- Font size / zoom
+  vim.keymap.set({ "n", "i", "x" }, "<C-=>", zoom_in, {
+    desc = "Increase Neovide font size",
+    silent = true,
+  })
+
+  vim.keymap.set({ "n", "i", "x" }, "<C-->", zoom_out, {
+    desc = "Decrease Neovide font size",
+    silent = true,
+  })
+
+  vim.keymap.set({ "n", "i", "x" }, "<C-0>", zoom_reset, {
+    desc = "Reset Neovide font size",
+    silent = true,
+  })
+end
--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,16 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- System clipboard.
+-- Makes normal y/p use the OS clipboard register "+".
+-- Example: yy copies to system clipboard, p pastes from it.
+vim.opt.clipboard = "unnamedplus"
+
+-- Neovide GUI settings
+if vim.g.neovide then
+  -- Change this font name if you use another Nerd Font.
+  -- Spaces can be written as underscores in guifont.
+
+  -- Initial Neovide zoom / scale.
+  vim.g.neovide_scale_factor = 1.0
+end
